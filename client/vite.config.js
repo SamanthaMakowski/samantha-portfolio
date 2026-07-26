23a823dba7a9623f4df816194edff608501ed4db
Samantha Makowski <samantha.makowskit@gmail.com>
2026-07-25 23:52:32 -0400

    fix vite preview allowed hosts for render deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,6 +3,11 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['samantha-portfolio-pdg5.onrender.com'],
+    port: 10000,
+    host: '0.0.0.0',
+  },
   server: {
     port: 3000,
     proxy: {
